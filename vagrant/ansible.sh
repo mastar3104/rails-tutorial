50ad97db65565e50358f2e5291fcbd2669c47e3d
sasajima <sasajima@m-field.co.jp>
2018-02-13 10:40:22 +0900

    [modify] directory change

--- vagrant/ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! [`which ansible`]; then
-    sudo yum install -y epel-release
-    sudo yum install -y ansible
-fi
-
-ansible-playbook -vv -i /vagrant/ansible/hosts /vagrant/ansible/playbook.yml
